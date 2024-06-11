a19558e2566d0a7a07fd97ff8c61a261a34be3e2
YechengYueEddy <yy4908@nyu.edu>
2024-06-11 21:13:04 +0800

    finished instructions

--- app_functions.py
+++ app_functions.py
@@ -14,6 +14,7 @@ def roll_die():
     :returns: the pseudo-random integer.
     """
     # complete this function below here
+    return random.randint(1, 6)
 
 
 def get_question_type():
@@ -24,6 +25,11 @@ def get_question_type():
     :returns: "sum" for an addition question, "difference" for a subtraction question.
     """
     # complete this function below here
+    random_int = random.randint(1, 6)
+    if random_int % 2 == 0:
+        return "sum"
+    else:
+        return "difference"
 
 
 def print_question(die_1_value, die_2_value, question_type):
@@ -43,6 +49,10 @@ def print_question(die_1_value, die_2_value, question_type):
     :returns: None
     """
     # complete this function below here
+    if question_type == "sum":
+        print(f"You rolled a {die_1_value} and a {die_2_value}... What is the sum of {die_1_value} and {die_2_value}?")
+    else:
+        print(f"You rolled a {die_1_value} and a {die_2_value}... What is the difference between {die_1_value} and {die_2_value}?")
 
 
 def input_answer():
@@ -56,6 +66,10 @@ def input_answer():
     :returns: The user's answer, as an int, if valid; or -1 if the user's response was not valid.
     """
     # complete this function below here
+    user_answer = input("Please enter the answer to the question: ").strip()
+    if not user_answer.isdigit():
+        return -1
+    return int(user_answer)
 
 
 def is_correct_answer(die_1_value, die_2_value, question_type, given_answer):
@@ -69,6 +83,16 @@ def is_correct_answer(die_1_value, die_2_value, question_type, given_answer):
     :returns: True if the user's given answer is correct, False otherwise.
     """
     # complete this function below here
+    if question_type == "sum":
+        if die_1_value + die_2_value == given_answer:
+            return True
+        else:
+            return False
+    else:
+        if abs(die_1_value - die_2_value) == given_answer:
+            return True
+        else:
+            return False
 
 
 def print_congratulations(question_type):
@@ -82,6 +106,10 @@ def print_congratulations(question_type):
     :param question_type: A string - either "sum" or "difference" - indicating whether the user was asked to add or subtract the two integers.
     """
     # complete this function below here
+    if question_type == "sum":
+        print("Yes! Congratulations on the successful addition!")
+    else:
+        print("Yes! Congratulations on the successful subtraction!")
 
 
 def print_correct_answer(die_1_value, die_2_value, question_type):
@@ -97,6 +125,12 @@ def print_correct_answer(die_1_value, die_2_value, question_type):
     :param question_type: A string - either "sum" or "difference" - indicating whether the user was asked to add or subtract the two integers.
     """
     # complete this function below here
+    if question_type == "sum":
+        correct_ans = die_1_value + die_2_value
+        print(f"No! The sum of {die_1_value} and {die_2_value} is {correct_ans}!")
+    else:
+        correct_ans = abs(die_1_value - die_2_value)
+        print(f"No! The difference between {die_1_value} and {die_2_value} is {correct_ans}!")
 
 
 def print_error_message():
@@ -107,3 +141,4 @@ def print_error_message():
     - "Sorry - that is an invalid answer.  Bye Bye!"
     """
     # complete this function below here
+    print("Sorry - that is an invalid answer.  Bye Bye!")
--- main.py
+++ main.py
@@ -23,6 +23,18 @@ def main():
     print("Welcome to the Math App!!!")
     print("")  # line break
     ### write code to complete this function BELOW here ###
+    dice_one = app_functions.roll_die()
+    dice_two = app_functions.roll_die()
+    question_type = app_functions.get_question_type()
+    app_functions.print_question(dice_one, dice_two, question_type)
+    user_ans = app_functions.input_answer()
+    if user_ans == -1:
+        app_functions.print_error_message()
+    is_correct = app_functions.is_correct_answer(dice_one, dice_two, question_type, user_ans)
+    if is_correct:
+        app_functions.print_congratulations(question_type)
+    else:
+        app_functions.print_correct_answer(dice_one, dice_two, question_type)
 
     ### write code to complete this function ABOVE here ###
     print("")  # line break
